0609f9e77c9400ff7f0bac3dc663736e2f5fb679
Naty Anastacio <147287067+NatyAnalytcs-1@users.noreply.github.com>
2025-06-16 10:59:33 -0300

    Update and rename queries to queries.sql

---
+++ 3_consultas/queries.sql
@@ -0,0 +1,22 @@
+</details> <details> <summary><strong>🔗 •	Quantos pedidos foram feitos por cada cliente? </strong></summary>
+
+
+</details> <details> <summary><strong>🔗 •	Algum vendedor também é fornecedor? </strong></summary>
+
+
+</details> <details> <summary><strong>🔗 •	Relação de produtos fornecedores e estoques </strong></summary>
+
+
+</details> <details> <summary><strong>🔗 •	Relação de nomes dos fornecedores e nomes dos produtos </strong></summary>
+
+
+</details> <details> <summary><strong>🔗 •	Relação de nomes dos fornecedores e nomes dos produtos </strong></summary>
+
+
+</details> <details> <summary><strong>🔗 •	Produtos com valores superiores a 100, ordenados decrescente </strong></summary>
+
+  
+</details> <details> <summary><strong>🔗 •	Clientes com mais de 1 pedido </strong></summary>
+
+  
+</details> <details> <summary><strong>🔗 •	Total gasto por cliente </strong></summary>
